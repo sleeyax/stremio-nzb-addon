8344ec1c560dbf8a72f3d1f2dc453775a73007d0
Sleeyax <yourd3veloper@gmail.com>
2025-11-28 16:23:01 +0100

    refactor: create shared addon builder

---
+++ src/addon.ts
@@ -0,0 +1,106 @@
+import {
+  AddonBuilder,
+  AddonInterface,
+  Manifest,
+  ManifestCatalog,
+  MetaPreview,
+  Stream,
+} from "@stremio-addon/sdk";
+import { NzbHydraAddonConfig, Item } from "./types.js";
+import { NZBWebApi } from "./nzb-api.js";
+import { imdbToTvdb, itemToStream } from "./utils.js";
+
+export function createAddonInterface(
+  manifest: Manifest,
+  catalog: ManifestCatalog,
+  name: string
+): AddonInterface {
+  const builder = new AddonBuilder(manifest);
+
+  builder.defineStreamHandler<NzbHydraAddonConfig>(
+    async ({ config, id, type }) => {
+      try {
+        const api = new NZBWebApi(config.indexerUrl, config.indexerApiKey);
+        let items: Item[] = [];
+
+        if (type === "movie") {
+          const imdbid = id.replace("tt", "");
+          const { channel } = await api.searchMovie(imdbid);
+          items = channel.item || [];
+        } else if (type === "series") {
+          const [imdbIdWithPrefix, season, episode] = id.split(":");
+          const imdbId = imdbIdWithPrefix.replace("tt", "");
+
+          const tvdbId = await imdbToTvdb(`tt${imdbId}`);
+          if (tvdbId) {
+            const { channel } = await api.searchSeries(tvdbId, season, episode);
+            items = channel.item || [];
+          } else {
+            console.warn(`Could not find TVDB ID for IMDB: tt${imdbId}`);
+          }
+        } else {
+          console.warn("Unsupported type:", type);
+          return { streams: [], cacheMaxAge: 0, staleRevalidate: 0 };
+        }
+
+        const nntpServers = config.nttpServers.map(({ server }) => server);
+        const streams: Stream[] = items.map((item) =>
+          itemToStream(item, nntpServers, name)
+        );
+
+        console.log(`Found ${streams.length} streams for ${type} ${id}`);
+
+        return { streams, cacheMaxAge: 0, staleRevalidate: 0 };
+      } catch (err: any) {
+        console.error(`Unexpected error in stream handler: ${err.message}`);
+        throw err;
+      }
+    }
+  );
+
+  builder.defineCatalogHandler<NzbHydraAddonConfig>(
+    async ({ config, extra: { search } }) => {
+      try {
+        if (search == null || search.trim() === "") {
+          console.warn("No search query provided");
+          return { metas: [], cacheMaxAge: 0, staleRevalidate: 0 };
+        }
+
+        const api = new NZBWebApi(config.indexerUrl, config.indexerApiKey);
+        const { channel } = await api.search(search);
+
+        const metas: MetaPreview[] = channel.item.map((item) => ({
+          id: `${catalog.id}:${encodeURIComponent(
+            item.enclosure["@attributes"].url
+          )}`,
+          type: "tv",
+          name: item.title,
+          description: item.description,
+        }));
+
+        return { metas, cacheMaxAge: 0, staleRevalidate: 0 };
+      } catch (err: any) {
+        console.error(`Unexpected error in catalog handler: ${err.message}`);
+        throw err;
+      }
+    }
+  );
+
+  builder.defineMetaHandler<NzbHydraAddonConfig>(async ({ id }) => {
+    try {
+      return {
+        meta: { id, name: "", type: "tv" },
+        cacheMaxAge: 0,
+        staleRevalidate: 0,
+        staleError: 0,
+      };
+    } catch (err: any) {
+      console.error(`Unexpected error in meta handler: ${err.message}`);
+      throw err;
+    }
+  });
+
+  const addonInterface = builder.getInterface();
+
+  return addonInterface;
+}
